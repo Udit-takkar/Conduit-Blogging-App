147ae9c256d4625a98211f4d91080191aa5285a4
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-07-11 01:28:02 +0530

    Fix API endpoijt

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,3 +1,4 @@
 import myLog from "../utils/myLog";
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const API_BASE_URL = "https://conduit-udit.herokuapp.com/api";
