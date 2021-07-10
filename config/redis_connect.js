a5a75e3cda8cf44d9c1fceee5ba447145c601564
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-07-11 00:09:44 +0530

    Redis Cloud Server added

--- config/redis_connect.js
+++ config/redis_connect.js
@@ -1,14 +1,19 @@
 const redis = require("redis");
+const { REDIS_ENDPOINT_URI, REDIS_PASSWORD } = process.env;
+const sanitizeRedisUrl = (url) => url.replace(/^(redis\:\/\/)/, "");
 
 // connect to redis
 // const redis_client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
-const redis_client = redis.createClient({
-  host:
-    process.env.NODE_ENV === "development"
-      ? process.env.REDIS_HOST
-      : "redis-server",
-  port: 6379,
-});
+// const redis_client = redis.createClient({
+//   host:
+//     process.env.NODE_ENV === "development"
+//       ? process.env.REDIS_HOST
+//       : "redis-server",
+//   port: 6379,
+// });
+const endpointUri = sanitizeRedisUrl(REDIS_ENDPOINT_URI);
+const password = REDIS_PASSWORD;
+const redisClient = redis.createClient(`redis://${endpointUri}`, { password });
 
 redis_client.on("connect", function () {
   console.log("redis client connected");
