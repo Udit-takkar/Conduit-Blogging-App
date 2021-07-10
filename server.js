2bd52be2c8b1bc63b92f90d38ce6541b8bd7c27a
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-07-11 01:18:58 +0530

    Fixing frontend URL

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
