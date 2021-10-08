1a3a0551ad5d301270861709d122a2d19bb1520c
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-10-08 18:07:06 +0530

    feat:article page refactored

--- client/src/components/Article Pages/Pagination.js
+++ client/src/components/Article Pages/Pagination.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
-import { navItems } from "../../features/articles/articleSlice";
-import { fetchArticlesByTag } from "../../features/articles/articleSlice";
 import { useHistory } from "react-router-dom";
 
-function Pagination({
-  articlesCount,
-  activePage,
-
-  tabName,
-}) {
-  const [isActive, setIsActive] = useState(activePage);
-  console.log(isActive);
-  const dispatch = useDispatch();
+function Pagination({ articlesCount, activePage, tabName }) {
   const history = useHistory();
-  useEffect(() => {
-    setIsActive(activePage);
-  }, [tabName, activePage]);
-  const tabs = useSelector(navItems);
+  const activePageNumber = activePage ?? 1;
   const NumberOfPages = [];
 
   for (let i = 1; i <= Math.ceil(articlesCount / 10); ++i) {
@@ -28,16 +14,6 @@ function Pagination({
     history.push({
       search: `?page=${page}`,
     });
-
-    // if (Component === "Home") {
-    //   if (tabName === "Global Feed" || tabName === "Your Feed") {
-    //     await dispatch(getPageArticles(page));
-    //   } else {
-    //     await dispatch(fetchArticlesByTag({ page, tag: tabName }));
-    //   }
-    // } else {
-    //   await getPageArticles(page);
-    // }
   };
   return (
     <PageContainer>
@@ -47,8 +23,9 @@ function Pagination({
             key={page}
             onClick={() => handlePages(page)}
             style={{
-              backgroundColor: isActive == page ? "#5cb85c" : "white",
-              color: isActive == page ? "white" : "black",
+              backgroundColor:
+                parseInt(activePageNumber) === page ? "#5cb85c" : "white",
+              color: parseInt(activePageNumber) === page ? "white" : "black",
             }}
           >
             {page}
---
+++ client/src/components/HomePage/index.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ArticleCard from "../Article Pages/ArticleCard";
+import {
+  getArticles,
+  getArticlesCount,
+  loading,
+} from "../../features/articles/articleSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Pagination from "../Article Pages/Pagination";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import { useHistory, useParams } from "react-router-dom";
+import HomeTabs from "../HomeTabs";
+
+function App({ isLoggedIn, activeItem, getPageArticles }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { search } = useLocation();
+  const { tagname } = useParams();
+
+  const { page } = queryString.parse(search);
+
+  const isLoading = useSelector(loading);
+
+  const articles = useSelector(getArticles);
+  const articlesCount = useSelector(getArticlesCount);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      console.log(getPageArticles);
+      await dispatch(getPageArticles({ page, tag: tagname ?? null }));
+    };
+    getArticles();
+  }, [page, tagname]);
+
+  const handleNavItemClick = async (item) => {
+    if (item === "Global Feed") {
+      history.push("/globalfeed");
+    } else if (item === "Your Feed") {
+      history.push("/myfeed");
+    }
+  };
+
+  return (
+    <HomePageContainer>
+      <HomeTabs
+        getActiveItem={activeItem}
+        handleNavItemClick={handleNavItemClick}
+        isLoggedIn={isLoggedIn}
+      />
+      <AppContainer>
+        <ArticlesContainer>
+          {articlesCount === 0 ? (
+            <>
+              {isLoading === true ? (
+                <Loader
+                  type="TailSpin"
+                  color="#5cb85c"
+                  height={80}
+                  width={80}
+                  style={{ marginTop: "50px" }}
+                />
+              ) : (
+                <p>No articles are here... yet.</p>
+              )}
+            </>
+          ) : (
+            <>
+              {articles?.map((article) => {
+                const {
+                  author: { username, image },
+                  title,
+                  description,
+                  favoritesCount,
+                  createdAt,
+                } = article;
+
+                return (
+                  <ArticleCard
+                    key={article.slug}
+                    username={username}
+                    image={image}
+                    title={title}
+                    description={description}
+                    createdAt={createdAt}
+                    favoritesCount={favoritesCount}
+                    slug={article.slug}
+                  />
+                );
+              })}
+            </>
+          )}
+        </ArticlesContainer>
+        <Pagination
+          articlesCount={articlesCount}
+          tabName={activeItem}
+          activePage={page}
+        />
+      </AppContainer>
+    </HomePageContainer>
+  );
+}
+
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ArticlesContainer = styled.div`
+  > p {
+    margin-top: 130px;
+  }
+`;
+export default App;
